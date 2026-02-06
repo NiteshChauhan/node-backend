ffd06e2ab3703936091ba12b4a849e4b444c349b
Niteshpal Singh <niteshsingh22002@gmail.com>
2026-02-06 19:44:39 +0530

    Fix CORS for serverless APIs

--- api/auth/register.js
+++ api/auth/register.js
@@ -1,7 +1,10 @@
 import { connectDB } from "../../lib/db.js";
 import { registerAdmin } from "../../controllers/authController.js";
+import { applyCors } from "../../lib/cors.js";
 
 export default async function handler(req, res) {
+  if (applyCors(req, res)) return;
+
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
